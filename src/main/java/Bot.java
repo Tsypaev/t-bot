dbccd499188924336823e45d6a13439c354284e1
tsypaev <tsypaevova@yandex.ru>
2020-02-19 19:05:26 +0500

    Init bot

---
+++ src/main/java/Bot.java
@@ -0,0 +1,32 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e);
+        }
+    }
+
+    public String getBotUsername() {
+        return "sdasdafasfasfbot";
+    }
+
+    public String getBotToken() {
+        return "995259227:AAE4Ru9x-4iq6SLiZcZ1yvzm8UDy2gncpm8";
+    }
+}
